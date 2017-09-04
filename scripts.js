effe273235cb6d67f201fd743ecbeea35760fee2
Eric Friday <ericfriday@gmail.com>
2017-09-04 11:18:04 -0500

    totalMonthlyCosts now adding up correctly

--- scripts.js
+++ scripts.js
@@ -1,25 +1,44 @@
 var employees = [];
+// var totalSalaries = 0;
+var totalMonthlyCosts = 0;
 
-$( document ).ready( docReady ) 
-
+$(document).ready(docReady);
 
 function docReady() {
-    $( '#submitEmployee' ).on ('click', addEmployee );
-} // end docReady
+    console.log('JQ Ready');
+    $('#submitEmployee').on('click', addEmployee);
+//    $('#submitEmployee').on('click', monthlyCosts);
+} // end docReady function
 
-function addEmployee( ) {
-    console.log('running addEmployee');
-    new Employee( $( '#firstName').val(), $( '#lastName').val(), $( '#idNumber').val(), $( '#jobTitle').val(), $( '#annualSalary').val() );
-}
+function addEmployee() {
+    //    console.log('running addEmployee');
+    new Employee($('#firstName').val(), $('#lastName').val(), $('#idNumber').val(), $('#jobTitle').val(), $('#annualSalary').val());
 
-function Employee(firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryIn){
+    // resetting fields to default attributes
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#idNumber').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
+} // end addEmployee function
+
+function Employee(firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryIn) {
     this.firstName = firstNameIn;
     this.lastName = lastNameIn;
     this.idNumber = idNumberIn;
     this.jobTitle = jobTitleIn;
     this.annualSalary = annualSalaryIn;
+    // making monthly salary calculation as a property of the employee
+    this.monthlySalary = Math.round(annualSalaryIn / 12);
+    // adding monthly salary to totalMonthlyCosts
+    totalMonthlyCosts += Number(this.monthlySalary);
     // add employee to employee array
-    employees.push( this );
-    console.log('pushing employee into employees array');
-    
+    employees.push(this);
+    //    console.log('pushing employee into employees array');
 } // end Employee constructor
+
+function monthlyCosts (employees, monthlySalary) {
+    return employees.reduce( function(a, b) {
+        return a + b[Employee.monthlySalary];
+    }, 0);
+} // end monthlyCosts function
